8b3f400890eed8fb0a257033bc7f08abdf0a32a3
Mateusz Buda <mateusz@antipattern.pl>
2016-05-24 19:54:36 +0200

    Decreased number of hidden layers to 3

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The job name is used to determine the name of job output and error files
-#SBATCH -J DT2118
+#SBATCH -J RBM.DT2118
 
 # Set the time allocation to be charged
 #SBATCH -A edu16.DT2118
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 
 # Maximum job elapsed time should be indicated whenever possible
-#SBATCH -t 05:00:00
+#SBATCH -t 07:00:00
 
 # Number of nodes that will be reserved for a given job
 #SBATCH --nodes=1
@@ -27,7 +27,7 @@ source modules_tegner
 
 (THEANO_FLAGS='device=gpu0' python $PDNNDIR/cmds/run_RBM.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --wdir ./nnet1 \
 --param-output-file nnet1/nnet1.mdl \
 --cfg-output-file nnet1/nnet1.cfg \
@@ -38,11 +38,11 @@ source modules_tegner
 THEANO_FLAGS='device=gpu0' python $PDNNDIR/cmds/run_DNN.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
 --valid-data "data/train_va_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --ptr-file nnet1/nnet1.mdl \
---ptr-layer-number 5 \
+--ptr-layer-number 3 \
 --wdir ./nnet1 \
---activation rectifier \
+--activation sigmoid \
 --lrate="C:0.16:15" \
 --momentum 0.9 \
 --batch-size 512 \
@@ -53,11 +53,11 @@ THEANO_FLAGS='device=gpu0' python $PDNNDIR/cmds/run_DNN.py \
 THEANO_FLAGS='device=gpu0' python $PDNNDIR/cmds/run_DNN.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
 --valid-data "data/train_va_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --ptr-file nnet1/nnet1.mdl \
---ptr-layer-number 5 \
+--ptr-layer-number 3 \
 --wdir ./nnet1/ \
---activation rectifier \
+--activation sigmoid \
 --lrate="C:0.004:10" \
 --momentum 0.9 \
 --batch-size 512 \
@@ -68,7 +68,7 @@ THEANO_FLAGS='device=gpu0' python $PDNNDIR/cmds/run_DNN.py \
 
 THEANO_FLAGS='device=gpu1' python $PDNNDIR/cmds/run_RBM.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --wdir ./nnet2 \
 --param-output-file nnet2/nnet2.mdl \
 --cfg-output-file nnet2/nnet2.cfg \
@@ -79,15 +79,15 @@ THEANO_FLAGS='device=gpu1' python $PDNNDIR/cmds/run_RBM.py \
 THEANO_FLAGS='device=gpu1' python $PDNNDIR/cmds/run_DNN.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
 --valid-data "data/train_va_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --ptr-file nnet2/nnet2.mdl \
---ptr-layer-number 5 \
+--ptr-layer-number 3 \
 --wdir ./nnet2 \
---activation rectifier \
+--activation sigmoid \
 --lrate="C:0.16:15" \
 --momentum 0.9 \
 --batch-size 512 \
---dropout-factor 0.2,0.2,0.2,0.2,0.2 \
+--dropout-factor 0.2,0.2,0.2 \
 --param-output-file nnet2/nnet2.mdl \
 --cfg-output-file nnet2/nnet2.cfg \
 |& tee -a nnet2/nnet2.log;
@@ -95,15 +95,15 @@ THEANO_FLAGS='device=gpu1' python $PDNNDIR/cmds/run_DNN.py \
 THEANO_FLAGS='device=gpu1' python $PDNNDIR/cmds/run_DNN.py \
 --train-data "data/train_tr_FBANK_D_A.pfile,context=5,random=true" \
 --valid-data "data/train_va_FBANK_D_A.pfile,context=5,random=true" \
---nnet-spec "792:2048:2048:2048:2048:2048:64" \
+--nnet-spec "792:2048:2048:2048:64" \
 --ptr-file nnet2/nnet2.mdl \
---ptr-layer-number 5 \
+--ptr-layer-number 3 \
 --wdir ./nnet2/ \
---activation rectifier \
+--activation sigmoid \
 --lrate="C:0.004:10" \
 --momentum 0.9 \
 --batch-size 512 \
---dropout-factor 0.2,0.2,0.2,0.2,0.2 \
+--dropout-factor 0.2,0.2,0.2 \
 --param-output-file nnet2/nnet2.mdl \
 --cfg-output-file nnet2/nnet2.cfg \
 |& tee -a nnet2/nnet2.log
